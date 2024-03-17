68bf62df0130c47acdf732215fc20017a5929d27
Sahil Singh <200104050@hbtu.ac.in>
2024-03-18 00:35:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -21,29 +21,34 @@ function App() {
     setSelectedItem(item);
     setIsOpen(false);
   };
+  useEffect(() => {
+    const filterProducts = (searchTerm) => {
+      let filteredProducts = [];    
+      if (searchTerm.trim() !== '' || searchTerm.trim() !== ' ') {
+        filteredProducts = products.filter(product =>
+          product.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      } else {
+        filteredProducts = [...products];
+      }
+      console.log(searchTerm);
+      if(!searchTerm){
+        setSelectedItem("All Category");
+      }else{
+        setProducts(filteredProducts);
+      }
+    };
 
-  const filterProducts = (searchTerm) => {
-    let filteredProducts = [];    
-    if (searchTerm.trim() !== '' || searchTerm.trim() !== ' ') {
-      filteredProducts = products.filter(product =>
-        product.title.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-    } else {
-      filteredProducts = [...products];
-    }
-    console.log(searchTerm);
-    if(!searchTerm){
-      setSelectedItem("All Category");
-    }else{
-      setProducts(filteredProducts);
-    }
-  };
+    filterProducts(searchTerm);
+  }, [searchTerm]);
+
+  
   
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        console.log(selectedItem);
+        // console.log(selectedItem);
         let response;
         if (selectedItem === "All Category") {
           response = await axios.get(`https://fakestoreapi.com/products`);
@@ -60,15 +65,12 @@ function App() {
     fetchProducts();
   }, [selectedItem]);
 
-  useEffect(() => {
-    filterProducts(searchTerm);
-  }, [searchTerm]);
 
   return (
     <>
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <a href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
