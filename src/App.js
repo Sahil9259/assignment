1be609443cfffed6b3f0597c311e747b51133311
Sahil Singh <200104050@hbtu.ac.in>
2024-03-18 01:19:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -75,12 +75,12 @@ function App() {
     <>
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a href="#"className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <button href="#"className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
             <span className="ml-3 text-xl">Tailblocks</span>
-          </a>
+          </button>
           <div className="relative md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
             <button
               className="bg-black text-white px-4 py-2 rounded flex items-center"
@@ -129,9 +129,9 @@ function App() {
                       <span className=" text-orange-500">Price
                       </span> $ {product.price}</h2>
                   </div>
-                  <a href="#" className=" mt-4 relative h-48 rounded overflow-hidden flex justify-center items-center">
+                  <div className=" mt-4 relative h-48 rounded overflow-hidden flex justify-center items-center">
                     <img alt="ecommerce" className=" h-full block" src={product.image} />
-                  </a>
+                  </div>
                 </div>
               ))
             ) : (
